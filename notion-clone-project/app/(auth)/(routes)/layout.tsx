62b8794089ba8a67bba257b6357fd15a58ae344a
Jack11140 <cho08232003@gmail.com>
2024-02-05 12:37:05 +0900

    02. 폴더 설정

---
+++ notion-clone-project/app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,7 @@
+// import React from 'react'
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="h-full text-white bg-blue-500">{children}</div>;
+};
+
+export default RootLayout;
