a79a592e1f2392e0d114fb0795e321368bd133fc
Jack11140 <cho08232003@gmail.com>
2024-02-05 12:16:26 +0900

    01. 환경 설정

---
+++ notion-clone-project/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hello Notion</p>
+      <Button>Button</Button>
+    </div>
+  );
+}
